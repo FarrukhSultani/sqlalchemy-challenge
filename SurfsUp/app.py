251ecc2a109bfbbeaabd28d92344e00fc690f78d
Farrukh Sultani <farrukh.sultani@gmail.com>
2023-03-06 23:44:31 -0800

    My app

---
+++ SurfsUp/app.py
@@ -0,0 +1,18 @@
+
+# Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Set up database engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect database and tables
+Base = automap_base()
+Base.prepare(engine, reflect=True)
