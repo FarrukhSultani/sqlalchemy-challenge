c68be8145eff28b7c392e5e875d6a57b2c14235b
Farrukh Sultani <farrukh.sultani@gmail.com>
2023-03-07 21:58:56 -0800

    making some progress toward the app development

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -3,24 +3,21 @@
 import datetime as dt
 import numpy as np
 import pandas as pd
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
+import datetime as dt
 from flask import Flask, jsonify
 
-# Set up database engine
-engine = create_engine("sqlite:///hawaii.sqlite")
-
-# Reflect database and tables
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-
-# Save references to tables
-Measurement = Base.classes.measurement
-Station = Base.classes.station
-
-# Create app instance
+# create an app
 app = Flask(__name__)
 
+# Define homepage route and list all available routes
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start_date<br/>"
+        f"/api/v1.0/start_date/end_date<br/>"
+    )
